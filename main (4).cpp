812d7151b19a725af165541d2046b051c9d91ccd
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:21:07 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,21 @@
+// Print the following pattern on the screen
+// **** 
+//  ** 
+//   *  
+//  ** 
+// ****
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ cout<<"****\n";
+ cout<<" **\n";
+ cout<<"  *\n";
+ cout<<" **\n";
+ cout<<"****\n";
+
+    return 0;
+}
