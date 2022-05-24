f6d8ca638fb951d840dad1b3815fa69f09bbb855
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:29:15 +0530

    Add files via upload

---
+++ main (9).cpp
@@ -0,0 +1,21 @@
+//Take two integer inputs from user. First calculate the sum of two then product of two. Finally, print the sum and product of both obtained results.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x;
+ int y;
+ cin>>x;
+ cin>>y;
+ 
+ int sum = x+y;
+ cout<<sum<<endl;
+  int product = x*y;
+  cout<<product;
+ 
+
+    return 0;
+}
