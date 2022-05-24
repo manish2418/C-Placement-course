d647d8a18c46ded4e8277567ee89d021882047d0
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:53:00 +0530

    Add files via upload

---
+++ main (15).cpp
@@ -0,0 +1,11 @@
+//Write a program to add an integer variable having value 5 and a double variable having value 6.2.
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int x=5;
+	double y=6.2;
+	double sum = x+y;
+	 cout<<sum;
+}
