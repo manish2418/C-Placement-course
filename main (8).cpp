9eefc93731ce07d1da60add0e19064a0b53ca3eb
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:27:08 +0530

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,20 @@
+// Write a C++ program to take two integer inputs from user and print sum and product of them.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x;
+ int y;
+ cin>>x;
+ cin>>y;
+ 
+ int sum = x+y;
+ cout<<sum;
+ 
+ 
+
+    return 0;
+}
