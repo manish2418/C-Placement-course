7eea1e0d9c04e125b420561ea4c060c806747ab1
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:45:12 +0530

    Add files via upload

---
+++ main (13).cpp
@@ -0,0 +1,12 @@
+//Write a program to assign a value of 100.235 to a double variable and then convert it to int.
+#include <iostream>
+int main()
+
+{
+ using namespace std;
+ double x;
+ x=2.23;
+ cout<<int(x);
+ 
+ return 0;
+}
