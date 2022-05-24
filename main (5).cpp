30d64a52053067d45943a9deb375ee0ad4810823
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:22:17 +0530

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,15 @@
+// Write a program to print Hello and CodesDope in two different lines.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ cout<<"Hello World\n";
+ cout<<"Code Dopes";
+ 
+
+    return 0;
+}
