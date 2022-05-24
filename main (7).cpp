e9d62952977fc98ad7a3f30f371d03a24d628031
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:25:30 +0530

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,18 @@
+// Store two integers in two variables x and y. Print the product of the two.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x=42;
+ int y=43;
+ 
+ int sum = x*y;
+ cout<<sum;
+ 
+ 
+
+    return 0;
+}
