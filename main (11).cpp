940775a18d1ff9d79ffdf73652f494a89070972d
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:37:16 +0530

    Add files via upload

---
+++ main (11).cpp
@@ -0,0 +1,20 @@
+
+//Write a C++ program to print an int, a double
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x=1;
+ double y=3.01;
+ 
+ 
+
+ cout<<x<<endl;
+ cout<<y;
+
+ 
+
+    return 0;
+}
