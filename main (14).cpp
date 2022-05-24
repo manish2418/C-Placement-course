f5d451c93ba941f627cb91a7c8ce647e46597b0e
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:50:28 +0530

    Add files via upload

---
+++ main (14).cpp
@@ -0,0 +1,9 @@
+//Write a program to add 3 to the ASCII value of the character 'd' and print the equivalent character.
+#include <iostream>
+int main()
+{
+	using namespace std;
+	char x = 'd'+3;
+	cout << x << endl;
+	return 0;
+}
