137edc9b7835cca3be80c1fd78786ab5f9f2f65c
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:08:31 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,20 @@
+
+// Write a C++ program to print
+// *
+// **
+// ***
+// ****
+// on screen. using only print 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"*\n";
+    cout<<"**\n";
+    cout<<"***\n";
+    cout<<"****\n";
+
+    return 0;
+}
