1d11aed4ffb95a18a8a0a04a0ba31133a2fe4157
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-24 22:31:48 +0530

    Add files via upload

---
+++ main (10).cpp
@@ -0,0 +1,20 @@
+//Write a program to take input of length and breadth of a rectangle from the user and print its area.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x;
+ int y;
+ cin>>x;
+ cin>>y;
+ 
+
+  int area = x*y;
+  cout<<area;
+ 
+
+    return 0;
+}
