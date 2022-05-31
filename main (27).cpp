51b3836633f43215a770511993049bf27829be39
Manish Kumar <mkdestiny2410@gmail.com>
2022-05-31 12:35:57 +0530

    Add files via upload

---
+++ main (27).cpp
@@ -0,0 +1,22 @@
+//Practise on Function
+//1.Sum of n natural number
+#include <iostream>
+
+using namespace std;
+
+int sum(int n){
+    int ans=0;
+    for(int i=1;i<=n;i++)
+        ans+=i;
+        return ans;
+}
+
+int main()
+{
+  int n;
+  cin>>n;
+  
+  cout<<sum(n)<<endl;
+
+    return 0;
+}
