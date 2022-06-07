cd922dbdd4d839c77e6c3d9c6dea5de01dc9f4cb
Manish Kumar <mkdestiny2410@gmail.com>
2022-06-07 12:36:21 +0530

    Add files via upload

---
+++ main (31).cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l,b;
+    cin>>l>>b;
+    if(l==b){
+        cout<<"square";
+    }else{
+        cout<<"NO";
+    }
+  
+
+    return 0;
+}
