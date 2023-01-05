07bdf3855196ad085c98c862b4b52056595c620c
Manish Kumar <mkdestiny2410@gmail.com>
2023-01-05 11:14:59 +0530

    Add files via upload

---
+++ Implementation of Binary Tree.cpp
@@ -0,0 +1,47 @@
+//Binary Tree
+
+
+#include <iostream>
+
+using namespace std;
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    
+    node (int d){
+    this->data=d;
+    this->left=NULL;
+    this->right=NULL;
+}
+};
+
+
+
+node* buildTree(node* root){
+    cout<<"Enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"Enter data for inserting at left: "<<endl;
+    root->left=buildTree(root->left);
+    cout<<"Enter data for inserting at right: "<<endl;
+    root->right=buildTree(root->right);
+    return root;
+    
+    
+}
+
+int main()
+{
+    node* root=NULL;
+    //creating tree
+    root=buildTree(root);
+
+    return 0;
+}
